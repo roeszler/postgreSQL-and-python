7a0b701a47a37bc166fa04c08155d3319a6fac39
Stuart Roeszler <stuart@roeszler.com>
2022-07-16 14:24:12 +0000

    add code to sql-expression.py

---
+++ sql-expression.py
@@ -0,0 +1,67 @@
+from sqlalchemy import (
+    create_engine, Table, Column, Float, ForeignKey, Integer, String, MetaData
+)
+
+# ------------ point and execute the instructions from our localhost "chinook" db
+db = create_engine("postgresql:///chinook")
+
+meta = MetaData(db) # contain a collection of data about our tables and the objects within those tables
+
+
+# ------------ Create variable artist_table for "Artist" table information
+artist_table = Table(
+    "Artist", meta,
+    Column("ArtistId", Integer, primary_key=True),
+    Column("Name", String)
+)
+
+
+# ------------ Create variable album_table for "Album" table information
+album_table = Table(
+    "Album", meta,
+    Column("AlbumId", Integer, primary_key=True),
+    Column("Title", String),
+    Column("ArtistId", Integer, ForeignKey("artist_table.ArtistId"))
+)
+
+
+# ------------ Create variable track_table for "Track" table information
+album_table = Table(
+    "Track", meta,
+    Column("TrackId", Integer, primary_key=True),
+    Column("Name", String),
+    Column("AlbumId", Integer, ForeignKey("album_table.AlbumId")),
+    # MediaTypeId and GenreId, Integers, are technically foreign keys, but we aren't defining all tables in this case, so we can simply set primary_key to False.
+    Column("MediaTypeId", Integer, primary_key=False),
+    Column("GenreId", Integer, primary_key=False),
+    Column("Composer", String),
+    Column("Milliseconds", String),
+    Column("Bytes", String),
+    Column("UnitPrice", Float)
+)
+
+
+# ------------ making the connection with .connect() method
+# This saves our connection to the database into a variable called 'connection'
+with db.connect() as connection:
+
+    # Query 1 - select all records from the "Artist" table
+    select_query = artist_table.select()
+
+    # Query 2 - select only the "Name" column from the "Artist" table
+    
+
+    # Query 3 - select only 'Queen' from the "Artist" table
+    
+
+    # Query 4 - select only by 'ArtistId' #51 from the "Artist" table
+    
+
+    # Query 5 - select only the albums with 'ArtistId' #51 on the "Album" table
+    
+
+    # Query 6 - select all tracks where the composer is 'Queen' from the "Track" table
+
+    results = connection.execute(select_query)
+    for result in results:
+        print(result)
