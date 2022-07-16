ba1d03b28ce58c48fb8d6475162243c0f6630a07
Stuart Roeszler <stuart@roeszler.com>
2022-07-16 08:47:15 +0000

    add search term variations sql-psycopg2.py file

--- sql-psycopg2.py
+++ sql-psycopg2.py
@@ -11,13 +11,36 @@ connection = psycopg2.connect(database="chinook")
 # ---------- a 'set' or 'list', similar to an 'array' in JavaScript
 cursor = connection.cursor()
 
-# ---------- Query 1 - select all records from the "Artist" table
-cursor.execute('SELECT * FROM "Artist"')
 
-# ---------- Fetch the results (multiple) from the cursor
+# # ---------- Query 1 - select all records from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist"')  # psycopg2 command syntax
+# # ---------- SELECT * FROM "Artist"; ------ SQL DB syntax
+
+# # ---------- Query 2 - select only the "Name" column from the "Artist" table
+# cursor.execute('SELECT "Name" FROM "Artist"')
+
+# # ---------- Query 3 - select only "Queen" from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "Name" = %s', ["Queen"])
+
+# # ---------- Query 4 - select only by PK "ArtistId" #51 from the "Artist" table
+# cursor.execute('SELECT * FROM "Artist" WHERE "ArtistId" = %s', [51])
+
+# # ---------- Query 5 - select only the albums with "ArtistId" #51 on the "Album" table
+# cursor.execute('SELECT * FROM "Album" WHERE "ArtistId" = %s', [51])
+
+# ---------- Query 6 - select all tracks where the composer is "Queen" from the "Track" table
+cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Queen"])
+
+# # ---------- Test 1 - select all tracks where the composer is "Black Sabbath" from the "Track" table
+# cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Black Sabbath"])
+
+# # ---------- Test 1 - select all tracks where the composer is "Test" from the "Track" table
+# cursor.execute('SELECT * FROM "Track" WHERE "Composer" = %s', ["Test"])
+
+# ---------- Fetch the results (multiple) from the cursor as tuple
 results = cursor.fetchall()
 
-# ---------- Fetch the results (single) from the cursor
+# ---------- Fetch the results (single) from the cursor as column
 # results = cursor.fetchone()
 
 # ---------- Close the connection
