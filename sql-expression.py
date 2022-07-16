2dbb14fc5491ca68340ad9694c4df0668c8e659e
Stuart Roeszler <stuart@roeszler.com>
2022-07-16 14:59:21 +0000

    add query codes to sql-expression.py

--- sql-expression.py
+++ sql-expression.py
@@ -26,7 +26,7 @@ album_table = Table(
 
 
 # ------------ Create variable track_table for "Track" table information
-album_table = Table(
+track_table = Table(
     "Track", meta,
     Column("TrackId", Integer, primary_key=True),
     Column("Name", String),
@@ -37,8 +37,8 @@ album_table = Table(
     Column("MediaTypeId", Integer, primary_key=False),
     Column("GenreId", Integer, primary_key=False),
     Column("Composer", String),
-    Column("Milliseconds", String),
-    Column("Bytes", String),
+    Column("Milliseconds", Integer),
+    Column("Bytes", Integer),
     Column("UnitPrice", Float)
 )
 
@@ -48,21 +48,22 @@ album_table = Table(
 with db.connect() as connection:
 
     # Query 1 - select all records from the "Artist" table
-    select_query = artist_table.select()
+    # select_query = artist_table.select()
 
     # Query 2 - select only the "Name" column from the "Artist" table
-    
+    # select_query = artist_table.select().with_only_columns([artist_table.c.Name])
 
     # Query 3 - select only 'Queen' from the "Artist" table
-    
+    # select_query = artist_table.select().where(artist_table.c.Name == "Queen")
 
     # Query 4 - select only by 'ArtistId' #51 from the "Artist" table
-    
+    # select_query = artist_table.select().where(artist_table.c.ArtistId == 51)
 
     # Query 5 - select only the albums with 'ArtistId' #51 on the "Album" table
-    
+    # select_query = album_table.select().where(album_table.c.ArtistId == 51)
 
     # Query 6 - select all tracks where the composer is 'Queen' from the "Track" table
+    select_query = track_table.select().where(track_table.c.Composer == "Queen")
 
     results = connection.execute(select_query)
     for result in results:
