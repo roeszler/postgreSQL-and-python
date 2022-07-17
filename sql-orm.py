a9216f8c02477c5a7a96e36385adb2ca20ebfa0c
Stuart Roeszler <stuart@roeszler.com>
2022-07-17 08:35:42 +0000

    add query 6 code to sql-orm.py

--- sql-orm.py
+++ sql-orm.py
@@ -79,21 +79,50 @@ base.metadata.create_all(db)
 
 # Query 1 - select all records from the "Artist" table
 
-artists = session.query(Artist) # session.query to query the Artist class above
-for artist in artists:  # to iterate over the results found
-    print(artist.ArtistId, artist.Name, sep=" | ") # print each separated columns using dot-notation and "|" on our for-loop
+# artists = session.query(Artist) # session.query to query the Artist class above
+# for artist in artists:  # to iterate over the results found
+#     print(artist.ArtistId, artist.Name, sep=" | ") # print each separated columns using dot-notation and "|" on our for-loop
 
 
 # Query 2 - select only the "Name" column from the "Artist" table
 
+# artists = session.query(Artist) 
+# for artist in artists:
+#     print(artist.Name)
+
 
 # Query 3 - select only "Queen" from the "Artist" table
+
+# To find a single artist, the new variable will be 'artist', singular.
+# This time, we can use the .filter_by() the Name column to equal Queen
+# The .first() method to only give us the first item from the query
+
+# artist = session.query(Artist).filter_by(Name="Queen").first()
+# print(artist.ArtistId, artist.Name, sep=" | ")
 
 
 # Query 4 - select only by "ArtistId" #51 from the "Artist" table
 
+# artist = session.query(Artist).filter_by(ArtistId=51).first()
+# print(artist.ArtistId, artist.Name, sep=" | ")
 
 # Query 5 - select only the albums with "ArtistId" #51 on the "Album" table
-
+# albums = session.query(Album).filter_by(ArtistId=51)
+# for album in albums:
+#     print(album.AlbumId, album.Title, album.ArtistId, sep=" | ")
 
 # Query 6 - select all tracks where the composer is "Queen" from the "Track" table
+tracks = session.query(Track).filter_by(Composer="Queen")
+for track in tracks:
+    print(
+        track.TrackId,
+        track.Name,
+        track.AlbumId,
+        track.MediaTypeId,
+        track.GenreId,
+        track.Composer,
+        track.Milliseconds,
+        track.Bytes,
+        track.UnitPrice,
+        sep=" | "
+    )
